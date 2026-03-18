78036b3eea343f0b849f5a3e28fc6a681b1c0ae6
samuelsrb <ssrbarroso@gmail.com>
2026-03-18 14:07:34 -0300

    add coins

--- scripts.js
+++ scripts.js
@@ -1,4 +1,10 @@
+const USD = 5.19;
+const EUR = 6;
+const GBP = 6.94;
+
+const form = document.querySelector("form");
 const amount = document.querySelector("#amount");
+const currency = document.querySelector("#currency");
 
 amount.addEventListener("input", (e) => {
   e.preventDefault();
@@ -6,3 +12,23 @@ amount.addEventListener("input", (e) => {
   const hasCharacterRegex = /\D+/g;
   amount.value = amount.value.replace(hasCharacterRegex, "");
 });
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  switch (currency.value) {
+    case "USD":
+      convertCurrency(amount.value, USD, "US$");
+      break;
+    case "EUR":
+      convertCurrency(amount.value, EUR, "€");
+      break;
+    case "GBP":
+      convertCurrency(amount.value, GBP, "£");
+      break;
+  }
+});
+
+function convertCurrency(amount, price, symbol) {
+  console.log(amount, price, symbol);
+}

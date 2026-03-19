08db0dde209924262c0ae42f033ec606cbaaee59
samuelsrb <ssrbarroso@gmail.com>
2026-03-19 14:53:54 -0300

    final

--- scripts.js
+++ scripts.js
@@ -5,6 +5,9 @@ const GBP = 6.94;
 const form = document.querySelector("form");
 const amount = document.querySelector("#amount");
 const currency = document.querySelector("#currency");
+const footer = document.querySelector("main footer");
+const description = document.querySelector("#description");
+const result = document.querySelector("#result");
 
 amount.addEventListener("input", (e) => {
   e.preventDefault();
@@ -30,5 +33,29 @@ form.addEventListener("submit", (e) => {
 });
 
 function convertCurrency(amount, price, symbol) {
-  console.log(amount, price, symbol);
+  try {
+    description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`;
+
+    let total = amount * price;
+
+    if (isNaN(total)) {
+      return alert("Por favor, digite o valor corretamente para converter.");
+    }
+
+    total = formatCurrencyBRL(total).replace("R$", "");
+    result.textContent = `${total} Reais`;
+
+    footer.classList.add("show-result");
+  } catch (e) {
+    footer.classList.remove("show-result");
+    console.log(e);
+    alert("Não foi possivel converter. Tente novamente mais tarde");
+  }
+}
+
+function formatCurrencyBRL(value) {
+  return Number(value).toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "BRL",
+  });
 }
